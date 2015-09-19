ee5c706b5cbf2e5c3b17cba35cdd06d9868191fe
Steven Fackler <sfackler@gmail.com>
2015-09-19 12:32:25 -0700

    Release v0.1.1

---
+++ doc/v0.1.1/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_binary_copy'] = ["impl&lt;'a, I: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_binary_copy/struct.BinaryCopyReader.html' title='postgres_binary_copy::BinaryCopyReader'>BinaryCopyReader</a>&lt;'a, I&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
