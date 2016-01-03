c5e8ab881480cecda5c4419bf97515a8b4a4bed3
Steven Fackler <sfackler@gmail.com>
2016-01-03 15:57:33 -0800

    Release v0.2.0

---
+++ doc/v0.2.0/implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_binary_copy'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='postgres_binary_copy/struct.WriteValueReader.html' title='postgres_binary_copy::WriteValueReader'>WriteValueReader</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
