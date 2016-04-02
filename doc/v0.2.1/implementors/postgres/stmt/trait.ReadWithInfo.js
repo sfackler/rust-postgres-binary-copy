9e2168ca2c33b99f4eb8985d022064165037f4fd
Steven Fackler <sfackler@gmail.com>
2016-04-02 16:09:59 -0700

    Release v0.2.1

---
+++ doc/v0.2.1/implementors/postgres/stmt/trait.ReadWithInfo.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_binary_copy'] = ["impl&lt;'a, I&gt; <a class='trait' href='https://sfackler.github.io/rust-postgres/doc/v0.11.5/postgres/stmt/trait.ReadWithInfo.html' title='postgres::stmt::ReadWithInfo'>ReadWithInfo</a> for <a class='struct' href='postgres_binary_copy/struct.BinaryCopyReader.html' title='postgres_binary_copy::BinaryCopyReader'>BinaryCopyReader</a>&lt;'a, I&gt; <span class='where'>where I: <a class='trait' href='postgres_binary_copy/trait.StreamingIterator.html' title='postgres_binary_copy::StreamingIterator'>StreamingIterator</a>&lt;Item=<a class='trait' href='https://sfackler.github.io/rust-postgres/doc/v0.11.5/postgres/types/trait.ToSql.html' title='postgres::types::ToSql'>ToSql</a>&gt;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
